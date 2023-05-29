83ef6179b1bb16ea3107c4eb040be03022592309
mbemelmans <mark.bemelmans@bristol.ac.uk>
2023-05-29 11:12:59 +0100

    Update Post_processing with reduced functions

--- SPOTSAR_main/Post_processing/__init__.py
+++ SPOTSAR_main/Post_processing/__init__.py
@@ -1,6 +1,4 @@
-from .get_off_files import get_off_files
-from .multikernel import multi_kernel
-from .read_lat_lon import read_lat_lon
+from .multikernel import MultiKernel
 from .reorder_files import reorder_files
-from .singlekernel import single_kernel
+from .singlekernel import SingleKernel
 
